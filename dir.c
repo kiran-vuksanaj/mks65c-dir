3d6c75ae31f071abc0b6242e5441ebf0ed86fb6d
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2019-11-15 13:37:53 -0500

    cycling through files

--- dir.c
+++ dir.c
@@ -8,6 +8,14 @@
 #include<dirent.h>
 
 int main() {
-  
+  DIR *cwd = opendir(".");
+  if(!cwd) {
+    printf("Error while opening .: [%d] - %s\n",errno,strerror(errno));
+  }
+  struct dirent *dirfile = readdir(cwd);
+  while(dirfile) {
+    printf("file\n");
+    dirfile = readdir(cwd);
+  }
   return 0;
 }
