7747aa706530fc17e75c0ef87dd3abd2abf886ca
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2019-11-15 13:54:54 -0500

    directory printing, file printing

--- dir.c
+++ dir.c
@@ -8,21 +8,29 @@
 #include<dirent.h>
 
 int main() {
+  int status;
   DIR *cwd = opendir(".");
   if(!cwd) {
     printf("Error while opening .: [%d] - %s\n",errno,strerror(errno));
+    return 1;
   }
   // printf("const DT_DIR: %hhd\n",DT_DIR);
   // printf("const DT_REG: %hhd\n",DT_REG);
   struct dirent *dirfile = readdir(cwd);
+  struct stat statbuf;
   while(dirfile) {
     // printf("file of type %hhd\n",dirfile->d_type);
     switch(dirfile->d_type) {
     case DT_DIR:
-      printf("is a directory\n");
+      printf("directory [%s]/\n",dirfile->d_name);
       break;
     case DT_REG:
-      printf("is a file\n");
+      status = stat(dirfile->d_name,&statbuf);
+      if(status < 0){
+	printf("Error while statting [%s]: [%d] - %s",dirfile->d_name,errno,strerror(errno));
+	break;
+      }
+      printf("%9o [%s] (%ld bytes)\n",statbuf.st_mode,dirfile->d_name,statbuf.st_size);
       break;
     default:
       printf("is something else\n");
