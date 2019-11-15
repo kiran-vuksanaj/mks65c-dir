46c71d9496d9229612944063174d4415a2dfe396
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2019-11-15 13:47:07 -0500

    switch for files/directories

--- dir.c
+++ dir.c
@@ -12,9 +12,22 @@ int main() {
   if(!cwd) {
     printf("Error while opening .: [%d] - %s\n",errno,strerror(errno));
   }
+  // printf("const DT_DIR: %hhd\n",DT_DIR);
+  // printf("const DT_REG: %hhd\n",DT_REG);
   struct dirent *dirfile = readdir(cwd);
   while(dirfile) {
-    printf("file\n");
+    // printf("file of type %hhd\n",dirfile->d_type);
+    switch(dirfile->d_type) {
+    case DT_DIR:
+      printf("is a directory\n");
+      break;
+    case DT_REG:
+      printf("is a file\n");
+      break;
+    default:
+      printf("is something else\n");
+      break;
+    }
     dirfile = readdir(cwd);
   }
   return 0;
