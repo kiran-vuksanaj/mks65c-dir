6394f6f231c2db0eaafc71d5a48ca946565f0659
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2019-11-15 15:05:40 -0500

    removed redundant stat

--- dir.c
+++ dir.c
@@ -3,7 +3,6 @@
 #include<errno.h>
 #include<string.h>
 
-#include<sys/stat.h>
 #include<sys/types.h>
 #include<dirent.h>
 
@@ -27,7 +26,6 @@ off_t ls(char *path) {
   // printf("const DT_DIR: %hhd\n",DT_DIR);
   // printf("const DT_REG: %hhd\n",DT_REG);
   struct dirent *dirfile = readdir(cwd);
-  struct stat statbuf;
   while(dirfile) {
     // printf("file of type %hhd\n",dirfile->d_type);
     switch(dirfile->d_type) {
@@ -35,11 +33,6 @@ off_t ls(char *path) {
       printf("directory [%s]/\n",dirfile->d_name);
       break;
     case DT_REG:
-      status = stat(dirfile->d_name,&statbuf);
-      if(status < 0){
-	printf("Error while statting [%s]: [%d] - %s",dirfile->d_name,errno,strerror(errno));
-	break;
-      }
       total += print_fstat(dirfile->d_name);
       break;
     default:
